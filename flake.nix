a7d8be406604f1ce45fcd3ac7b21014900f61209
Neil Hemmerly <neilhemm0@gmail.com>
2024-07-24 20:12:13 -0400

    replace flake and add .envrc

--- flake.nix
+++ flake.nix
@@ -1,36 +1,37 @@
 {
-  description = "C++ Development";
+  description = "A Nix-flake-based C/C++ development environment";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages.default = pkgs.hello;
-
-	devShells.default = pkgs.mkShell {
-	  packages = with pkgs; [
-		boost
-		catch2
-		cmake
-		gdb
-		vscode-fhs
-	  ];
-	};
-      };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
-
-      };
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell.override
+          {
+            # Override stdenv in order to change compiler:
+            # stdenv = pkgs.clangStdenv;
+          }
+          {
+            packages = with pkgs; [
+              clang-tools
+              cmake
+              codespell
+              conan
+              cppcheck
+              doxygen
+              gtest
+              lcov
+              vcpkg
+              vcpkg-tool
+            ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
+          };
+      });
     };
 }
+
